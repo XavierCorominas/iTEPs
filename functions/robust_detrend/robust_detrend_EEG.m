2dce2fb3366c8c33226476984100c159e29d2550
XavierCorominas <109666629+XavierCorominas@users.noreply.github.com>
2025-10-06 11:10:16 +0200

    Update robust_detrend_EEG.m

--- functions/robust_detrend/robust_detrend_EEG.m
+++ functions/robust_detrend/robust_detrend_EEG.m
@@ -2,7 +2,6 @@ function [EEG_out] = robust_detrend_EEG(EEG_in, polynomial_order)
 
 % This function wraps up the robust detrend code provided by Johanna Metsomaa. 
 
-% Tuomas Mutanen
 
 EEG_out = EEG_in;
 data_in = EEG_in.data;
